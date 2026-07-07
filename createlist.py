4b51325533751570aaafcd4ef89ca861e44bd7ec
Ann Elsa Joy <projectannelsa@gmail.com>
2026-07-07 15:46:28 +0530

    Day 2 task added

---
+++ createlist.py
@@ -0,0 +1,24 @@
+# Creating a list
+fruits = ["Apple", "Banana", "Orange"]
+print(fruits)
+
+
+#accessing elements in a list
+fruits = ["Apple", "Banana", "Orange"]
+print("First element:", fruits[0])
+print("Second element:", fruits[1])
+print("Last element:", fruits[-1])
+
+
+#slicing
+numbers = [10, 20, 30, 40, 50]
+print(numbers[1:4])
+print(numbers[:3])
+print(numbers[2:])
+
+#updating an element in a list 
+fruits = ["Apple", "Banana", "Orange"]
+fruits[1] = "Mango"
+print(fruits)
+
+
